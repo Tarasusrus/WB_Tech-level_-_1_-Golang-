e710e6dde2413186356046d5df0737e40c63d5c2
tarasusrus <bar.norilsk@gmail.com>
2023-12-21 12:39:26 +0300

    Update Human struct, add Word Reversing function and update README.md
    Refactored the Human struct to include a 'sex' field and created a 'defaultHuman' constructor for it. The 'Speak' method was updated to encompass this new field. Additionally, a new function 'ReverseWords' was introduced to reverse words in a string. Enhanced the 'README.md' file with additional insights on Go's runtime features.

--- prob/1.HumanStruct.go
+++ prob/1.HumanStruct.go
@@ -1,43 +1,40 @@
-/*
-
-Дана структура Human (с произвольным набором полей и методов). Реализовать встраивание методов в структуре Action
-от родительской структуры Human (аналог наследования).
-
-*/
-
 package main
 
 import "fmt"
 
+const WelcomeString = "Привет, эта функция определена в родительском классе и возвращает данные: "
+
 type Human struct {
 	Name string
 	Age  int
+	Sex  string
+}
+
+func DefaultHuman(name string) Human {
+	h := Human{Name: name, Age: 45, Sex: "Мужской"} // использование одиночной присваивающей операции
+	return h
 }
 
 func (h *Human) Speak() string {
-	return "Привет эта функция определена в родительском классе и возвращает имя " + h.Name
+	return WelcomeString + h.Name
 }
 
-/*
-Теперь, если мы хотим создать структуру Action,
-которая встраивает Human и добавляет свои уникальные поля или методы:
-*/
-
 type Action struct {
-	// Чтобы методы родительского класса были доступнты мы наследуем встраивая в структуру это реализация одного из приципов ООП
-	Human
-	Action string
+	Human             // Реализация наследования путем встраивания в структуру
+	ActionDescription string
 }
 
 func (a Action) PerformAction() string {
-	return " я вызываю метод унаследованную от класса Human" + a.Name + " выполняется в " + a.Action
+	return "Я вызываю метод, унаследованный от класса Human. " + a.Name + " выполняет действие: " + a.ActionDescription
 }
 
 func main() {
-	a := Action{
-		Human{Name: "Taras", Age: 33},
-		"Метод определенный в структуре Action",
+	person := DefaultHuman("Иван Иванов")
+	action := Action{
+		Human{Name: "Тарас", Age: 33, Sex: "Мужской"},
+		"Метод, определенный в структуре Action",
 	}
-	fmt.Println(a.Speak())         // Вызов метода Speak от Human
-	fmt.Println(a.PerformAction()) // Вызов метода PerformAction от Action
+	fmt.Println(action.Speak())         // вызываем метод Speak() от Human
+	fmt.Println(action.PerformAction()) // вызываем метод PerformAction() от Action
+	fmt.Println(person.Speak())         // вызываем метод Speak() от Human
 }
